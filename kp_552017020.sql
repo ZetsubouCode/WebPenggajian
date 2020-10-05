b99a59bad4abca64ca987889dd85896a0a6ee76b
ZetsubouCode <kignatius80@gmail.com>
2020-10-05 11:12:03 +0700

    renew database

--- kp_552017020.sql
+++ kp_552017020.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 10, 2020 at 05:53 PM
+-- Generation Time: Oct 05, 2020 at 03:54 AM
 -- Server version: 5.5.32
 -- PHP Version: 5.4.19
 
@@ -21,6 +21,38 @@ SET time_zone = "+00:00";
 --
 CREATE DATABASE IF NOT EXISTS `kp_552017020` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
 USE `kp_552017020`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_absensi_mentor`
+--
+
+CREATE TABLE IF NOT EXISTS `tb_absensi_mentor` (
+  `id_absen` int(50) NOT NULL AUTO_INCREMENT,
+  `bulan` varchar(10) NOT NULL,
+  `tahun` varchar(4) NOT NULL,
+  `jumlah_anak` int(50) NOT NULL,
+  `kehadiran_anak` int(50) NOT NULL,
+  `j_kehadirananak` int(50) NOT NULL,
+  `j_kunjungan` int(50) NOT NULL,
+  `j_pertemuan` int(50) NOT NULL,
+  `lesson` int(50) NOT NULL,
+  `evaluasi` int(50) NOT NULL,
+  `inskeh` int(50) NOT NULL,
+  `insfile` int(50) NOT NULL,
+  `id_gaji` int(50) NOT NULL,
+  `nip` int(30) NOT NULL,
+  PRIMARY KEY (`id_absen`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+
+--
+-- Dumping data for table `tb_absensi_mentor`
+--
+
+INSERT INTO `tb_absensi_mentor` (`id_absen`, `bulan`, `tahun`, `jumlah_anak`, `kehadiran_anak`, `j_kehadirananak`, `j_kunjungan`, `j_pertemuan`, `lesson`, `evaluasi`, `inskeh`, `insfile`, `id_gaji`, `nip`) VALUES
+(1, 'maret', '2020', 6, 1, 5, 4, 4, 3, 123, 321, 333, 2, 1506969),
+(2, 'Januari', '2020', 12, 44, 1, 5, 4, 10, 122, 221, 12, 6, 1504149);
 
 -- --------------------------------------------------------
 
@@ -95,6 +127,7 @@ INSERT INTO `t_pegawai` (`nip`, `nama_pegawai`, `tgl_lhr`, `tlp`, `alamat`, `id_
 (1504142, 'komisi', '2017-10-19', '085766876123', 'salatiga', '1'),
 (1504146, 'mulyani', '0000-00-00', '085728845849', 'boyolali', '2'),
 (1504149, 'Budi', '2014-05-01', '08123456789', 'Semarang', '4'),
+(1506969, 'Bambang', '2020-10-01', '1234565432', 'jakarta', '6'),
 (15013178, 'bob', '0000-00-00', '081222333444', 'jl ahmad yani', '3');
 
 -- --------------------------------------------------------
@@ -105,34 +138,24 @@ INSERT INTO `t_pegawai` (`nip`, `nama_pegawai`, `tgl_lhr`, `tlp`, `alamat`, `id_
 
 CREATE TABLE IF NOT EXISTS `t_penggajian` (
   `no_penggajian` int(20) NOT NULL AUTO_INCREMENT,
-  `tanggal_penggajian` date DEFAULT NULL,
-  `bulan` varchar(10) NOT NULL,
-  `tahun` varchar(4) NOT NULL,
-  `nip` varchar(15) NOT NULL,
-  `Evaluasi` int(50) DEFAULT NULL,
-  `inkeh` int(50) DEFAULT NULL,
-  `insfile` int(50) DEFAULT NULL,
-  `kontribusi_gereja` int(20) DEFAULT NULL,
-  `kontribusi_yci` int(20) DEFAULT NULL,
-  `jumlah_anak` int(30) DEFAULT NULL,
-  `kehadiran_anak` int(30) DEFAULT NULL,
-  `j_kehadirananak` int(30) DEFAULT NULL,
-  `j_kunjungan` int(30) DEFAULT NULL,
-  `j_pertemuan` int(30) DEFAULT NULL,
-  `lesson` int(30) DEFAULT NULL,
-  `id_gaji` varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`no_penggajian`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=12 ;
+  `tanggal_penggajian` date NOT NULL,
+  `keterangan_gaji` varchar(10) NOT NULL,
+  `slip_mentor` int(30) DEFAULT NULL,
+  `slip_staff` int(30) DEFAULT NULL,
+  PRIMARY KEY (`no_penggajian`),
+  KEY `slip_mentor` (`slip_mentor`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=13 ;
 
 --
 -- Dumping data for table `t_penggajian`
 --
 
-INSERT INTO `t_penggajian` (`no_penggajian`, `tanggal_penggajian`, `bulan`, `tahun`, `nip`, `Evaluasi`, `inkeh`, `insfile`, `kontribusi_gereja`, `kontribusi_yci`, `jumlah_anak`, `kehadiran_anak`, `j_kehadirananak`, `j_kunjungan`, `j_pertemuan`, `lesson`, `id_gaji`) VALUES
-(5, '2020-08-02', 'Desember', '2022', '1504146', 0, 0, 0, 500000, 750000, 0, NULL, NULL, NULL, NULL, NULL, NULL),
-(9, '2020-08-28', 'Februari', '2020', '1504149', 100000, 50000, 15000, NULL, NULL, 2, 1, 0, 2, 3, 8, 'Mentor Kelas Khusus'),
-(10, '2020-09-06', 'Agustus', '2020', '1504142', NULL, NULL, NULL, 9999, 6666, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
-(11, '2020-09-07', 'Januari', '2020', '1504146', NULL, NULL, NULL, 9999, 888, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+INSERT INTO `t_penggajian` (`no_penggajian`, `tanggal_penggajian`, `keterangan_gaji`, `slip_mentor`, `slip_staff`) VALUES
+(5, '2020-08-02', '', 0, 1),
+(9, '2020-08-28', '', 0, 2),
+(10, '2020-09-06', '', 0, 3),
+(11, '2020-09-07', '', 1, 0),
+(12, '2020-10-04', '', NULL, 4);
 
 -- --------------------------------------------------------
 
@@ -141,23 +164,55 @@ INSERT INTO `t_penggajian` (`no_penggajian`, `tanggal_penggajian`, `bulan`, `tah
 --
 
 CREATE TABLE IF NOT EXISTS `t_tarif_mentor` (
-  `id_gaji` varchar(100) NOT NULL,
+  `id` int(50) NOT NULL AUTO_INCREMENT,
   `gaji` double NOT NULL,
   `bonus` double DEFAULT NULL,
   `kunjungan` int(20) NOT NULL,
   `tutorial` int(20) NOT NULL,
   `lesson` int(20) NOT NULL,
-  PRIMARY KEY (`id_gaji`),
-  UNIQUE KEY `id_gaji` (`id_gaji`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `evaluasi` int(50) NOT NULL,
+  `inskeh` int(50) NOT NULL,
+  `insfile` int(50) NOT NULL,
+  `id_jabatan` int(30) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_gaji` (`id`),
+  UNIQUE KEY `id_jabatan` (`id_jabatan`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
 
 --
 -- Dumping data for table `t_tarif_mentor`
 --
 
-INSERT INTO `t_tarif_mentor` (`id_gaji`, `gaji`, `bonus`, `kunjungan`, `tutorial`, `lesson`) VALUES
-('IPIA', 123, 333, 421, 11, 3333),
-('Mentor Kelas Khusus', 35000, 688, 25000, 8000, 8000);
+INSERT INTO `t_tarif_mentor` (`id`, `gaji`, `bonus`, `kunjungan`, `tutorial`, `lesson`, `evaluasi`, `inskeh`, `insfile`, `id_jabatan`) VALUES
+(2, 35000, 688, 25000, 8000, 8000, 0, 0, 0, 6),
+(6, 123, 123, 123, 123, 123, 123, 123, 123, 4);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `t_tarif_staff`
+--
+
+CREATE TABLE IF NOT EXISTS `t_tarif_staff` (
+  `id` int(50) NOT NULL AUTO_INCREMENT,
+  `bulan` varchar(10) NOT NULL,
+  `tahun` varchar(4) NOT NULL,
+  `gereja` int(50) NOT NULL,
+  `yci` int(50) NOT NULL,
+  `nip` int(30) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=19 ;
+
+--
+-- Dumping data for table `t_tarif_staff`
+--
+
+INSERT INTO `t_tarif_staff` (`id`, `bulan`, `tahun`, `gereja`, `yci`, `nip`) VALUES
+(1, 'maret', '2020', 99997, 88888, 1504142),
+(2, 'september', '2020', 696969, 777776, 1504146),
+(3, 'juni', '2020', 56746574, 3324242, 15013178),
+(6, 'Januari', '2020', 444, 555, 1504146),
+(17, 'Februari', '2020', 123, 123, 1504146);
 
 -- --------------------------------------------------------
 
@@ -165,14 +220,15 @@ INSERT INTO `t_tarif_mentor` (`id_gaji`, `gaji`, `bonus`, `kunjungan`, `tutorial
 -- Stand-in structure for view `view_gaji_staff`
 --
 CREATE TABLE IF NOT EXISTS `view_gaji_staff` (
-`No` int(20)
-,`Tanggal` date
-,`Bulan` varchar(10)
-,`Tahun` varchar(4)
-,`Nama Pegawai` varchar(35)
-,`NIP` varchar(15)
-,`Kontribusi Gereja` int(20)
-,`Kontribusi YCI` int(20)
+`no_penggajian` int(20)
+,`nip` int(30)
+,`nama_pegawai` varchar(35)
+,`nama_jabatan` varchar(100)
+,`tanggal_penggajian` date
+,`bulan` varchar(10)
+,`tahun` varchar(4)
+,`gereja` int(50)
+,`yci` int(50)
 );
 -- --------------------------------------------------------
 
@@ -196,7 +252,7 @@ CREATE TABLE IF NOT EXISTS `view_pengguna` (
 --
 DROP TABLE IF EXISTS `view_gaji_staff`;
 
-CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `view_gaji_staff` AS select `pg`.`no_penggajian` AS `No`,`pg`.`tanggal_penggajian` AS `Tanggal`,`pg`.`bulan` AS `Bulan`,`pg`.`tahun` AS `Tahun`,`p`.`nama_pegawai` AS `Nama Pegawai`,`pg`.`nip` AS `NIP`,`pg`.`kontribusi_gereja` AS `Kontribusi Gereja`,`pg`.`kontribusi_yci` AS `Kontribusi YCI` from ((`t_penggajian` `pg` join `t_pegawai` `p` on((`p`.`nip` = `pg`.`nip`))) join `tb_jabatan` `j` on((`j`.`kode` = `p`.`id_jabatan`))) where (not((`j`.`kategori_jabatan` like 'Mentor')));
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `view_gaji_staff` AS select `pg`.`no_penggajian` AS `no_penggajian`,`s`.`nip` AS `nip`,`p`.`nama_pegawai` AS `nama_pegawai`,`j`.`nama_jabatan` AS `nama_jabatan`,`pg`.`tanggal_penggajian` AS `tanggal_penggajian`,`s`.`bulan` AS `bulan`,`s`.`tahun` AS `tahun`,`s`.`gereja` AS `gereja`,`s`.`yci` AS `yci` from (((`t_penggajian` `pg` join `t_tarif_staff` `s` on((`s`.`id` = `pg`.`slip_staff`))) join `t_pegawai` `p` on((`p`.`nip` = `s`.`nip`))) join `tb_jabatan` `j` on((`j`.`kode` = `p`.`id_jabatan`))) where (not((`j`.`kategori_jabatan` like 'Mentor')));
 
 -- --------------------------------------------------------
 
